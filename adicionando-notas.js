ce649b4cdf6245ce9c6b9dec317c61882e060dfe
iranilsj <irani.fpo@gmail.com>
2023-10-29 23:14:05 -0300

    Exemplo para adicionar elemento no array ja definido

---
+++ adicionando-notas.js
@@ -0,0 +1,6 @@
+const notas = [10, 6, 8];
+//  push -> adicionar um elemento dentro do array
+notas.push(7)
+// adicionar elemento dentro da função com o indice numerado na sequencia
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media);
