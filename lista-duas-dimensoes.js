bba83ffac61ad059bed34677b070ae4209f6cb28
iranilsj <irani.fpo@gmail.com>
2023-10-31 15:46:03 -0300

    Exemplo de lista de duas dimenções em forma de matriz

--- lista-duas-dimensoes.js
+++ lista-duas-dimensoes.js
@@ -1,6 +1,6 @@
 const alunos = ["João", "Juliana",  "Ana", "Caio"];
 const medias = [10, 8, 7.5, 9];
 
-const listaDealunosEMedias = [alunos, medias];
+const listaDeAlunosEMedias = [alunos, medias];
 console.log(`A aluna da posição 1 da lista de alunos é: ${listaDealunosEMedias[0][1]}.
 A nota da ${listaDealunosEMedias[0][1]} é: ${listaDealunosEMedias[1][1]}`);
