b4091d63fc129802b9ba62d5b1cfff20d2e996b8
iranilsj <irani.fpo@gmail.com>
2023-10-31 00:29:35 -0300

    Exemplo para juntar salar (juntar  listas)

---
+++ juntando-salas.js
@@ -0,0 +1,5 @@
+const salaJS = ["Evaldo", "Camila", "Mariana"];
+const salaPython  = ["Juliana", "Léonardo", "Raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+console.log(salasUnificadas);
