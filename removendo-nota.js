60e8a047c0abcb7a3b6a4e258cfeb43ae979bc8c
iranilsj <irani.fpo@gmail.com>
2023-10-31 10:47:58 -0300

    Exemplo para remover elemento da lista

--- removendo-nota.js
+++ removendo-nota.js
@@ -1,5 +1,6 @@
 const notas = [10, 6, 8, 5.5, 10];
 // para remover um dado da lista/array 
 notas.pop();
+console.log(notas)
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 console.log(`A média é ${media}`);
