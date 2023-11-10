9e45239cde1c832ad32bfa779b25b40270d74f11
iranilsj <irani.fpo@gmail.com>
2023-11-10 02:06:39 -0300

    Exemplo de string maiusculas + diferencas  de callback + consoles

---
+++ map-nomes.js
@@ -0,0 +1,11 @@
+// Padronizar nomes apenas com letras maiusculas
+const nomes = ['ana julia', 'Caio', 'vinicius', 'BIA', 'iran'];
+
+// outra opcao sem colocar o "Return" eliminando as chaves 
+const nomesPadronizados = nomes.map((nome) =>  nome.toUpperCase());
+
+// 1) Exemplo de console.log  forma em  lista
+console.log(nomesPadronizados);
+
+// 2) Exemplo de console.log  forma de impressão diferentes ( Forma escrita )
+console.log(`${nomesPadronizados}`);
